6e0c9b0395179b10d0e971c8291201ca9313cb7f
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-06-27 12:47:53 -0400

    Added a fix for a bad url replacement that may have snuck into production.

--- src/admin/__tests__/TestBuilder.test.tsx
+++ src/admin/__tests__/TestBuilder.test.tsx
@@ -16,15 +16,13 @@ function renderComponent(props?:Partial<TestBuilderProps>) {
 
 }
 
-
 describe("TestBuilder", () => {
     it("renders", () => {
-        // renderComponent();
-        // expect(screen.queryByPlaceholderText(REPLACE_PLACEHOLDER)).toBeInTheDocument()
-        // expect(screen.queryByPlaceholderText(FIND_PLACEHOLDER)).toBeInTheDocument()
-        // expect(screen.queryAllByPlaceholderText(BAD_TEST_PLACEHOLDER).length).toBe(2);
-        // expect(screen.queryAllByPlaceholderText(GOOD_TEST_PLACEHOLDER).length).toBe(2);
+        renderComponent();
+        expect(screen.queryByPlaceholderText(REPLACE_PLACEHOLDER)).toBeInTheDocument()
+        expect(screen.queryByPlaceholderText(FIND_PLACEHOLDER)).toBeInTheDocument()
+        expect(screen.queryAllByPlaceholderText(BAD_TEST_PLACEHOLDER).length).toBe(2);
+        expect(screen.queryAllByPlaceholderText(GOOD_TEST_PLACEHOLDER).length).toBe(2);
     });
     test.todo("Updates ")
-
 })
--- src/publish/fixesAndUpdates/validations/proxyServerLinkValidation.ts
+++ src/publish/fixesAndUpdates/validations/proxyServerLinkValidation.ts
@@ -3,7 +3,7 @@ import {badContentFixFunc, badContentRunFunc, CourseValidation} from "./";
 import {IContentHaver} from "../../../canvas/course/courseTypes";
 
 
-const oldProxyRegex =/proxy1\.unity\.edu/g;
+const oldProxyRegex =/(proxy1\.unity\.edu|unity.idn.oclc.org)/g;
 const newProxyReplace = 'unity.idm.oclc.org';
 export const proxyServerLinkValidation: CourseValidation<IContentHaver> = {
     name: "Proxy Server Link Validation",
