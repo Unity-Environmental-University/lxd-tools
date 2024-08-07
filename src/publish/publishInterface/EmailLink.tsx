ea2a3ceb7edf33734fd2080aeb344038a000e08e
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-08-07 10:22:50 -0400

    Fixed a Denial of Service vulnerability by upgrading postcss dependency.

--- src/publish/publishInterface/EmailLink.tsx
+++ src/publish/publishInterface/EmailLink.tsx
@@ -4,9 +4,10 @@ import {renderToString} from "react-dom/server";
 import React, {useState} from "react";
 import {Course} from "../../canvas/course/Course";
 import {useEffectAsync} from "@/ui/utils";
-import {PUBLISH_FORM_EMAIL_TEMPLATE_URL} from "@/consts";
+import {DOCUMENTATION_TOC_URL, DOCUMENTATION_TOPICS_URL, PUBLISH_FORM_EMAIL_TEMPLATE_URL} from "@/consts";
 import {Alert} from "react-bootstrap";
 import {ITermData} from "@/canvas/Term";
+import {fetchJson} from "@/canvas/fetch/fetchJson";
 
 type EmailLinkProps = {
     user: IUserData,
@@ -30,13 +31,14 @@ export function EmailLink({user, emails, course, termData, sectionStart}: EmailL
 
     const bcc = emails.join(',');
     const subject = encodeURIComponent(course.name?.replace('BP_', '') + ' Section(s) Ready Notification');
-    const [emailTemplate, setEmailTemplate] = useState<string|undefined>();
+    const [emailTemplate, setEmailTemplate] = useState<string | undefined>();
+    const [additionsTemplate, setAdditionsTemplate] = useState<string | undefined>();
     const [errorMessages, setErrorMessages] = useState<string[]>([]);
 
     useEffectAsync(async () => {
-        if(emailTemplate) return;
+        if (emailTemplate) return;
         const emailResponse = await fetch(PUBLISH_FORM_EMAIL_TEMPLATE_URL);
-        if(!emailResponse.ok) {
+        if (!emailResponse.ok) {
             setErrorMessages([emailResponse.statusText, await emailResponse.text()])
             return;
         }
@@ -46,20 +48,26 @@ export function EmailLink({user, emails, course, termData, sectionStart}: EmailL
     }, [])
 
 
+    useEffectAsync(async () => {
+        if (course.baseCode) {
+            const additionsTemplate = await getAdditionsTemplate(course.baseCode);
+            setAdditionsTemplate(additionsTemplate);
+        }
+    }, [course])
 
     async function copyToClipboard() {
-        if(!emailTemplate) {
+        if (!emailTemplate) {
             setErrorMessages([`Can't find template email to fill at ` + PUBLISH_FORM_EMAIL_TEMPLATE_URL]);
             return;
         }
         const body = renderEmailTemplate(emailTemplate, {
-        userName: user.name,
-        userTitle: "Learning Experience Designer",
-        courseCode: course.parsedCourseCode?.replace('BP_', '') ?? '[[CODE NOT FOUND]]',
-        termName:termData ? termData.name : '[[TERM NAME]]',
-        courseStart: getCourseStart(),
-        publishDate: getPublishDate(),
-    })
+            userName: user.name,
+            userTitle: "Learning Experience Designer",
+            courseCode: course.parsedCourseCode?.replace('BP_', '') ?? '[[CODE NOT FOUND]]',
+            termName: termData ? termData.name : '[[TERM NAME]]',
+            courseStart: getCourseStart(),
+            publishDate: getPublishDate(),
+        })
         await navigator.clipboard.write([
             new ClipboardItem({
                 'text/html': new Blob([body], {type: 'text/html'})
@@ -88,7 +96,7 @@ export function EmailLink({user, emails, course, termData, sectionStart}: EmailL
     return <>
         <a href={`mailto:${user.email}?subject=${subject}&bcc=${bcc}`}>{emails.join(', ')}</a>
         {termData && <button onClick={copyToClipboard}>Copy Form Email to Clipboard</button>}
-        { errorMessages.map(msg => <Alert>{msg}</Alert>) }
+        {errorMessages.map(msg => <Alert>{msg}</Alert>)}
     </>
 }
 
@@ -102,7 +110,57 @@ export type EmailTextProps = {
     publishDate: string,
 }
 
-export function renderEmailTemplate(emailTemplate:string, props: EmailTextProps) {
+export function renderEmailTemplate(emailTemplate: string, props: EmailTextProps, additions?: string[]) {
     let renderedTemplate = emailTemplate.split('\n').slice(1).join('\n')
+    additions ??= [];
+    const additionsString = additions.join('\n');
+    renderedTemplate = renderedTemplate.replace('{additions}', additionsString)
     return Object.entries(props).reduce((accumulator, [key, value]) => accumulator.replaceAll(`{{${key}}}`, value), renderedTemplate)
+}
+
+
+async function getSpecificTemplates() {
+
+    const tocResponse = await fetch(DOCUMENTATION_TOPICS_URL + '/lxd.tree');
+    if (!tocResponse.ok) {
+        console.log(`Documentation not found as ${DOCUMENTATION_TOPICS_URL}`)
+        return [] as string[];
+    }
+    const text = await tocResponse.text();
+    const tocXml = new DOMParser().parseFromString(text, 'text/xml') as XMLDocument;
+
+    const child = tocXml.firstChild;
+    if (!child) return;
+    console.log([...tocXml.childNodes.entries()]);
+
+    const result = tocXml.evaluate('//toc-element', tocXml, null, XPathResult.FIRST_ORDERED_NODE_TYPE);
+    const formEmailNode = result.singleNodeValue as Element;
+    const children = formEmailNode?.getElementsByTagName('toc-element');
+    if (children) return [...children]
+        .map(a => a.getAttribute('topic'))
+        .filter(b => b !== null) as string[];
+    return [] as string[];
+}
+
+
+
+export async function getAdditionsTemplate(courseCode: string) {
+    const templates = await getSpecificTemplates();
+    const topic = templates && templates.find(topic => topic.toLocaleLowerCase().includes(courseCode.toLocaleLowerCase()))
+    const url = `${DOCUMENTATION_TOPICS_URL}/${topic}`
+    if(!topic || !url) return;
+    const response = await fetch(url);
+    if(!response.ok) throw new TemplateNotFoundError(courseCode);
+    const additionsTemplate = await response.text();
+    let renderedTemplate = additionsTemplate.split('\n').slice(1).join('\n')
+    return renderedTemplate;
+}
+
+export class TemplateNotFoundError extends Error {
+    name = 'TemplateNotFoundError'
+    code:string
+    constructor(code:string, message?: string) {
+        super(message ?? code);
+        this.code = code;
+    }
 }
--- src/publish/publishInterface/__tests__/EmailLink.test.tsx
+++ src/publish/publishInterface/__tests__/EmailLink.test.tsx
@@ -1,8 +1,38 @@
-import {EmailTextProps, renderEmailTemplate} from "@/publish/publishInterface/EmailLink";
+import {
+    EmailTextProps, getAdditionsTemplate,
+    renderEmailTemplate, TemplateNotFoundError
+} from "@/publish/publishInterface/EmailLink";
 import {render} from "@testing-library/react";
-import publishEmailMock, {mockFilled, mockValues} from "@/publish/publishInterface/__mocks__/publishEmailMock";
+import publishEmailMock, {
+    mockCourseSpecificContent,
+    mockFilled, mockFilledSpecific, mockTocXml,
+    mockValues
+} from "@/publish/publishInterface/__mocks__/publishEmailMock";
+import {DOCUMENTATION_TOPICS_URL} from "@/consts";
+import * as EmailLinkApi from '../EmailLink';
+import {mockCourseData} from "@/canvas/course/__mocks__/mockCourseData";
+global.fetch = jest.fn();
+const fetchMock = fetch as jest.Mock;
+
+describe('Renders Email template', () => {
+    it('render emails template', () => {
+        expect(renderEmailTemplate(publishEmailMock, mockValues)).toEqual(mockFilled);
+    })
+
+    test('render email with course specific info', async() => {
+
+        fetchMock.mockResolvedValueOnce( { ok: true, text: async () => mockTocXml });
+        fetchMock.mockResolvedValueOnce({ ok: true, text: async () => mockCourseSpecificContent })
+        const additionsTemplate = await getAdditionsTemplate('TEST000') as string;
+
+        expect(renderEmailTemplate(
+            publishEmailMock,
+            mockValues,
+            [additionsTemplate],
+        )).toEqual(mockFilledSpecific);
+    });
+
+});
 
 
-test('render email template', () => {
-    expect(renderEmailTemplate(publishEmailMock, mockValues)).toEqual(mockFilled);
-})
+
