4d6cf0b8adf2513615191ff4ab58c07e0be9fb05
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-12-09 15:10:41 -0500

    Added validation for enabling text submission across assignments Added validation for disabling student discussion editing and creation

--- src/publish/fixesAndUpdates/validations/assignments/index.tsx
+++ src/publish/fixesAndUpdates/validations/assignments/index.tsx
@@ -1,5 +1,7 @@
 import emptyAssignmentCategories from "@publish/fixesAndUpdates/validations/assignments/emptyAssignmentCategories";
+import textSubmissionEnabled from "@publish/fixesAndUpdates/validations/assignments/textSubmissionEnabled";
 
 export default [
     emptyAssignmentCategories,
+    textSubmissionEnabled,
 ]
