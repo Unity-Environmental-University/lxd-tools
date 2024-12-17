30ffef0f5b65b39ebbd0619b43092d42cf961886
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-12-17 16:25:47 -0500

    version 2.7.8.6 for testing

--- src/publish/fixesAndUpdates/validations/assignments/index.tsx
+++ src/publish/fixesAndUpdates/validations/assignments/index.tsx
@@ -1,7 +1,9 @@
 import emptyAssignmentCategories from "@publish/fixesAndUpdates/validations/assignments/emptyAssignmentCategories";
 import textSubmissionEnabled from "@publish/fixesAndUpdates/validations/assignments/textSubmissionEnabled";
+import textSubEnabledBug from "@publish/fixesAndUpdates/validations/assignments/textSubEnabledBug";
 
 export default [
     emptyAssignmentCategories,
     textSubmissionEnabled,
+    textSubEnabledBug,
 ]
