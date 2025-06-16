3752fcde1ab0696d025dee0fec24797945847459
Hallie <hlarsson@unity.edu>
2025-06-16 15:17:26 -0400

    Refactor date handling in syllabus updates to include graduate course logic and improve component structure

---
+++ src/declarations.d.ts
@@ -0,0 +1,15 @@
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare global {
+    interface Global {
+        TextEncoder: typeof TextEncoder;
+    }
+}
+
+declare module '*.html' {
+  const content: string;
+  export default content;
+}
