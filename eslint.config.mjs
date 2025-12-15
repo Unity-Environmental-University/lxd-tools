ad75732e2ccdef7be34723d3bf3ed31047b5f1fb
grsiekman <gsiekman@unity.edu>
2025-12-15 11:55:15 -0500

    Update eslint.config.mjs
    Disabled the no-undef error because it's flagging InsertPosition as undefined. This shouldn't cause any issues because TypeScript will still flag improperly defined variables.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,14 +15,11 @@ export default tseslint.config(
         rules: {
             '@typescript-eslint/no-unused-vars': 'warn',
             '@typescript-eslint/no-explicit-any': 'off', // Allow any types in test files
-            "@/no-undef": "error", // TypeScript specific: disallow use of undeclared variables
+            //"@/no-undef": "error", // TypeScript specific: disallow use of undeclared variables
             //"@/no-unused-vars": "warn", // Warn for unused variables, ignore those starting with '_'
         }
     },
     {
         files: ['**/*.test.ts', '**/*.test.tsx'], // Adjust the glob pattern if necessary
     },
-    {
-        env: { browser: true },
-    }
 )
