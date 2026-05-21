8c820301ecf1cf85c5e3a88f8d42b9313bac79a4
grsiekman <greg@siekmang.com>
2026-05-21 16:07:32 -0400

    Introducing syllabusStore and SyllabusSync
    syllabusStore brought over from a different branch to start fresh,
    SyllabusSync introduced. Currently a WIP, but in time will do a simple
    swap and then develop into giving users an insight into the differences
    between the template and course syllabi and give them the option to
    adopt/reject those changes. syllabus button added to addButtons.

---
+++ src/publish/fixesAndUpdates/syllabusStore.ts
@@ -0,0 +1,60 @@
+import { ISyllabusHaver } from "@ueu/ueu-canvas";
+import { create } from "zustand";
+
+// TODO; This maybe should go somewhere else in file hierarchy
+
+type SyllabusChange = {
+  originalSnippet: string;
+  updatedSnippet: string;
+};
+
+type SyllabusState = {
+  originalHtml: string;
+  draftHtml: string;
+  changes: SyllabusChange[];
+  status: "idle" | "fetching" | "loaded" | "saving" | "success" | "error";
+  results: string[];
+  fetchSyllabus: (course: ISyllabusHaver) => Promise<void>;
+  setDraftHtml: (html: string) => void;
+  updateSyllabus: (course: ISyllabusHaver, html: string) => Promise<void>;
+};
+
+export const useSyllabusStore = create<SyllabusState>((set, get) => ({
+  originalHtml: "", // What is pulled from Canvas
+  draftHtml: "", // The currently modified version
+  changes: [],
+  status: "idle",
+  results: [], // List of discovered issues
+
+  fetchSyllabus: async (course: ISyllabusHaver) => {
+    if (get().originalHtml !== "") return;
+
+    set({ status: "fetching", originalHtml: "", draftHtml: "" });
+    try {
+      const syllabus = await course.getSyllabus();
+      set({ originalHtml: syllabus, draftHtml: syllabus, status: "loaded" });
+    } catch (e) {
+      set({ status: "error", results: ["Error fetching syllabus: ", e as string] });
+    }
+  },
+
+  setDraftHtml: (html: string) => {
+    set({ draftHtml: html });
+  },
+
+  updateSyllabus: async (course: ISyllabusHaver, html: string) => {
+    set({ status: "saving" });
+
+    if (html === get().originalHtml) {
+      set({ status: "success", results: ["No changes needed!"] });
+      return;
+    }
+
+    try {
+      await course.changeSyllabus(html);
+      set({ status: "success" });
+    } catch (e) {
+      set({ status: "error", results: ["Error saving syllabus: ", e as string] });
+    }
+  },
+}));
