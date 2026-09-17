96b9df91796f63f3e847a8509245b65db1881177
Greg <github@siekmang.com>
2026-09-17 09:23:21 -0400

    Added courseContent back to validations index

--- src/publish/fixesAndUpdates/validations/index.ts
+++ src/publish/fixesAndUpdates/validations/index.ts
@@ -1,9 +1,11 @@
 //import {dontUseThisValidation} from "@publish/fixesAndUpdates/dontUseThis";
 
 import { kalturaSizeTests } from "./kalturaSizeFix";
+import courseContent from "./courseContent";
 
 export default [
 	kalturaSizeTests,
+	courseContent,
   //...references,
   //   dontUseThisValidation
 ];
