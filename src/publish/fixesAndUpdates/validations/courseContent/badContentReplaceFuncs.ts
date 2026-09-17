6d1aa338df5c7fb236039f5e98bccd7dfd182157
Greg <github@siekmang.com>
2026-09-17 10:39:42 -0400

    Re-addbadContentReplaceFuncs

---
+++ src/publish/fixesAndUpdates/validations/courseContent/badContentReplaceFuncs.ts
@@ -0,0 +1,20 @@
+import {IContentHaver} from "@ueu/ueu-canvas/course/courseTypes";
+import {BaseContentItem} from "@ueu/ueu-canvas/content/BaseContentItem";
+import {badContentFixFunc, badContentRunFunc} from "@publish/fixesAndUpdates/validations/utils";
+
+export default function badContentReplaceFuncs<
+    CourseType extends IContentHaver,
+    ContentType extends BaseContentItem,
+>(
+    badTest: RegExp,
+    replace: string,
+    getContentFunc?: (course:CourseType) => Promise<ContentType[]>
+) {
+
+    return {
+        run: badContentRunFunc<CourseType, ContentType>(badTest, getContentFunc),
+        fix: badContentFixFunc<CourseType, ContentType>(badTest, replace, getContentFunc)
+    }
+}
+
+
